1acf5a032e6757c0206df8d96b79d47a2af34378
Daniel <daniellanguilller@gmail.com>
2019-12-28 13:58:15 +1100

    first draft

--- js/app.js
+++ js/app.js
@@ -1,71 +1,96 @@
-let NUM_LIVES = 5;
-
 const startButton = document.getElementsByClassName("btn__reset")[0];
 const startDiv = document.getElementById("overlay");
-startButton.addEventListener("click", () => {
-  startDiv.style.display = "none";
-});
-
-// parent div of hearts
-const heartDiv = document.getElementById("scoreboard");
-appendHearts(heartDiv, NUM_LIVES);
-
-const phraseDiv = document.getElementById("phrase");
-const wholePhrase = phrases[Math.floor(Math.random() * phrases.length)];
-console.log("Whole phrase: ", wholePhrase);
-let phrasör = wholePhrase.split("");
-console.log("Phrasör split: ", phrasör);
-
-let htmlToRender = document.createElement("ul");
-phrasör.forEach(el => {
-  if (el === " ") {
-    let appendör = document.createElement("li");
-    appendör.innerHTML = " ";
-    appendör.className = "space";
-    return htmlToRender.appendChild(appendör);
+// let NUM_LIVES = 5;
+startButton.addEventListener("click", e => {
+  if (e.target.textContent === "Reset") {
+    location.reload(true);
+  } else {
+    runGame(5);
   }
-  let appendör = document.createElement("li");
-  appendör.innerHTML = el;
-  appendör.className = "letter";
-  return htmlToRender.appendChild(appendör);
 });
-phraseDiv.appendChild(htmlToRender);
 
-phrasör = removeChars(phrasör, " ");
+function runGame(numLives) {
+  let lives = numLives;
+  console.log("Num lives refresh: ", lives);
+  startDiv.style.display = "none";
 
-const keyBoardDiv = document.getElementById("qwerty");
-const keyBoardButtons = keyBoardDiv.getElementsByTagName("button");
+  // parent div of hearts
+  const heartDiv = document.getElementById("scoreboard");
+  appendHearts(heartDiv, lives);
 
-for (let el of keyBoardDiv.getElementsByTagName("button")) {
-  el.addEventListener("click", () => {
-    el.disabled = true;
-    el.classList.add("chosen");
+  const phraseDiv = document.getElementById("phrase");
+  let wholePhrase = phrases[Math.floor(Math.random() * phrases.length)];
+  console.log("Whole phrase: ", wholePhrase);
+  let phrasör = wholePhrase.split("");
+  console.log("Phrasör split: ", phrasör);
 
-    if (!phrasör.includes(el.innerHTML)) {
-      NUM_LIVES--;
-      appendHearts(heartDiv, NUM_LIVES);
-      if (NUM_LIVES < 1) {
-        // logic to game over here.
-        startDiv.style.display = "";
-        startDiv.classList.add("lose");
-        console.log("lose");
-      }
+  let htmlToRender = document.createElement("ul");
+  phrasör.forEach(el => {
+    if (el === " ") {
+      let appendör = document.createElement("li");
+      appendör.innerHTML = " ";
+      appendör.className = "space";
+      return htmlToRender.appendChild(appendör);
     }
+    let appendör = document.createElement("li");
+    appendör.innerHTML = el;
+    appendör.className = "letter";
+    return htmlToRender.appendChild(appendör);
+  });
 
-    for (let square of htmlToRender.children) {
-      if (square.innerHTML === el.innerHTML) {
-        square.classList.add("show");
-        phrasör = removeChars(phrasör, el.innerHTML);
+  while (phraseDiv.firstChild) {
+    phraseDiv.removeChild(phraseDiv.firstChild);
+  }
+  phraseDiv.appendChild(htmlToRender);
+
+  phrasör = removeChars(phrasör, " ");
+
+  const keyBoardDiv = document.getElementById("qwerty");
+
+  //refresh classnames of keyboard;
+  for (let el of keyBoardDiv.getElementsByTagName("button")) {
+    el.disabled = false;
+    el.classList.remove("chosen");
+  }
+
+  for (let el of keyBoardDiv.getElementsByTagName("button")) {
+    el.addEventListener("click", () => {
+      console.log("Num_lives: ", lives);
+      el.disabled = true;
+      el.classList.add("chosen");
+
+      if (!phrasör.includes(el.innerHTML)) {
+        lives--;
         console.log("new phrasor: ", phrasör);
-        if (phrasör.length === 0) {
-          //logic to win gayme here
+        appendHearts(heartDiv, lives);
+        if (lives < 1) {
+          // logic to game over here.
           startDiv.style.display = "";
-          startDiv.classList.add("win");
-          console.log("Winner");
+          startDiv.classList.add("lose");
+          startDiv.firstElementChild.innerHTML = "You Lost!";
+          startButton.innerHTML = "Reset";
+          //   console.log("lose");
+          return;
         }
       }
-    }
-  });
+
+      for (let square of htmlToRender.children) {
+        if (square.innerHTML === el.innerHTML) {
+          square.classList.add("show");
+          phrasör = removeChars(phrasör, el.innerHTML);
+          console.log("new phrasor: ", phrasör);
+          if (phrasör.length === 0) {
+            //logic to win gayme here
+            startDiv.style.display = "";
+            startDiv.classList.add("win");
+            startDiv.firstElementChild.innerHTML = "You Won!";
+            startButton.innerHTML = "Reset";
+            return;
+          }
+        }
+      }
+    });
+  }
 }
 
 function removeChars(baseArray, charToRemove) {
